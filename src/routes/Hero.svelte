ec7d8e2db925e7c01f6babeb13253afe1375a699
tengkuhafidz <tengkuhafidz@gmail.com>
2023-11-05 18:41:42 +0800

    remove profile pic

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,11 +1,11 @@
 <div
   class="p-8 text-center text-white bg-gradient-to-r from-slate-800 to-slate-900"
 >
-  <img
+  <!-- <img
     src="https://media.publit.io/file/projectassets/profile-1.webp"
     class="rounded-xl h-20 border-2 mx-auto md:h-24"
     alt="profile pic"
-  />
+  /> -->
   <h1 class="text-2xl font-bold mt-2 md:text-3xl py-1 text-teal-500">
     Jariyah Apps 📱
   </h1>

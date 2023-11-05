0de38a18aefb2cc63f1c587297232e1363251254
tengkuhafidz <tengkuhafidz@gmail.com>
2023-11-05 18:39:54 +0800

    brand as jariyah apps

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -6,10 +6,8 @@
     class="rounded-xl h-20 border-2 mx-auto md:h-24"
     alt="profile pic"
   />
-  <h1 class="text-2xl font-bold mt-2 md:text-3xl py-1">
-    Tengku Hafidz <span class="underline decoration-wavy text-teal-500"
-      >Projects
-    </span> 🧑🏾‍💻
+  <h1 class="text-2xl font-bold mt-2 md:text-3xl py-1 text-teal-500">
+    Jariyah Apps 📱
   </h1>
   <p class="font-light md:text-xl">Hope you find something of benefit</p>
 </div>
